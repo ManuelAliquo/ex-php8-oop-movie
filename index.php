c83f0805130687a0ca143b35be66486e981c9355
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-05-07 19:48:58 +0200

    add Movie class Objects

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ class Genre
 {
     public $name;
     public $movieCount;
+
+    function __construct($_name, $_movieCount)
+    {
+        $this->name = $_name;
+        $this->movieCount = $_movieCount;
+    }
 };
 
 class Movie
@@ -12,7 +18,21 @@ class Movie
     public $year;
     public $director;
     public $genre;
+
+    function __construct($_title, $_year, $_director, Genre $_genre)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->director = $_director;
+        $this->genre = $_genre;
+    }
 }
+
+$theLordOfTheRings = new Movie("The Lord of the Rings", 2001, "Peter Jackson", new Genre("Fantasy", 124));
+$projectHailMary = new Movie("Project Hail Mary", 2026, "Phil Lord and Chris Miller", new Genre("Sci-Fi", 163));
+
+var_dump($theLordOfTheRings);
+var_dump($projectHailMary);
 
 ?>
 
